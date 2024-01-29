0d2e3c248b9f28eff940000b60e7fc7cfeffe7e5
bishopone <abeselomsolomon106@gmail.com>
2024-01-29 20:42:07 +0000

    hosted !!!

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2');
 
+// const db = mysql.createConnection({
+//   user: 'root',
+//   host: 'localhost',
+//   password: '',
+//   database: 'room_managment', 
+// });
 const db = mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: '',
-  database: 'room_managment', 
+  host: "localhost",
+  user: "aboldefo_abol",
+  password: "7K7js{$[TUHf",
+  database: 'aboldefo_room_managment', 
 });
 
 module.exports = db;
