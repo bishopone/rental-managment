49cb459816e9c1df0e70e796ecb83e697c8076ea
bishopone <abeselomsolomon106@gmail.com>
2024-03-28 07:38:34 +0000

    deployment fixing

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,16 +1,16 @@
 const corsOptions = [
     {
+        origin: 'https://royalbusinesses.net/',
+        credentials: true,
+        optionSuccessStatus: 200
+    },
+    {
         origin: 'https://shop-64682.web.app/',
         credentials: true,
         optionSuccessStatus: 200
     },
     {
         origin: 'http://localhost:3000/',
-        credentials: true,
-        optionSuccessStatus: 200
-    },
-    {
-        origin: 'https://royalbusinesses.net/',
         credentials: true,
         optionSuccessStatus: 200
     },
