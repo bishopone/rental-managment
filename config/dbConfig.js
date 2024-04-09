fd5640a27e0f765d1438f5c82cc1990f8780db4e
bishopone <abeselomsolomon106@gmail.com>
2024-04-09 19:14:20 +0000

    multer

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -8,11 +8,19 @@ const mysql = require('mysql2');
 //   namedPlaceholders: true
 // });
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "royalbti_royalbti",
+//   password: "neln3Jt}zc(V",
+//   database: 'royalbti_room_managment',
+//   namedPlaceholders: true
+// });
+
 const db = mysql.createConnection({
   host: "localhost",
-  user: "royalbti_royalbti",
-  password: "neln3Jt}zc(V",
-  database: 'royalbti_room_managment',
+  user: "aboldefo_abol",
+  password: "7K7js{$[TUHf",
+  database: "aboldefo_room_managment", 
   namedPlaceholders: true
 });
 
