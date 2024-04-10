212f430321c8a83f0ae2d031935602ede792df87
bishopone <abeselomsolomon106@gmail.com>
2024-04-10 08:27:00 +0000

    the domain trial

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: '',
-  database: 'room_managment',
-  namedPlaceholders: true
-});
-
 // const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "royalbti_royalbti",
-//   password: "neln3Jt}zc(V",
-//   database: 'royalbti_room_managment',
+//   user: 'root',
+//   host: 'localhost',
+//   password: '',
+//   database: 'room_managment',
 //   namedPlaceholders: true
 // });
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "royalbti_royalbti",
+  password: "neln3Jt}zc(V",
+  database: 'royalbti_room_managment',
+  namedPlaceholders: true
+});
 
 // const db = mysql.createConnection({
 //   host: "localhost",
